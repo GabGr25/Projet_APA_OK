59cc97d432c46beb9833c2e51cf35b03d4a0fe2b
Gabriel Gressier <gabriel_gressier@ens.univ-artois.fr>
2021-11-24 14:09:29 +0100

    Ajout de la classe CompteurTableau

---
+++ src/CompteurTableau.java
@@ -0,0 +1,13 @@
+public class CompteurTableau extends Compteur {
+    public final int TAILLE_INITIALE = 100;
+    private Mot[] elements;
+
+
+    public void addOccurrence(String mot) {
+        TO DO;
+    }
+
+    public CompteurTableau (String fichierTexte) {
+        TO DO
+    }
+}
